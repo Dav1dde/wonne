646ff4b61a9aee996ab873e90501b425ddf7c4d2
David <admin@dav1d.de>
2012-12-27 16:50:26 +0100

    optimization in awe_call

--- wonne/util.d
+++ wonne/util.d
@@ -6,41 +6,49 @@ private {
     import wonne.string;
     import wonne.webview;
     
-    import std.typetuple : TypeTuple, allSatisfy;
+    import std.typetuple : TypeTuple, allSatisfy, anySatisfy;
     import std.traits : ReturnType, Unqual;
     import std.conv : to;
 }
 
 
 template is_awe_string(T...) if(T.length == 1) {
-    enum is_awe_string = (is(T == string) || is(T == wstring) || is(T == AWEString) || is(T == awe_string*));
+    enum is_awe_string = (is(T[0] == string) || is(T[0] == wstring) || is(T[0] == AWEString) || is(T[0] == awe_string*));
 }
 
 template is_awe_string(T...) if(T.length > 1) {
     alias allSatisfy!(is_awe_string, T) is_awe_string;
 }
 
+template is_awe_d_string(T) {
+    enum is_awe_d_string = (is(T == string) || is(T == wstring));
+}
+
 /// Automatically converts a string for wstring into an AWEString and frees it after calling func.
 /// It also converts the returntype of the function into a wonne abstraction or a d-type.
 auto awe_call(alias func, Args...)(Args args) {
-    alias convert_strings!(Args) NewArgs;
+    static if(anySatisfy!(is_awe_d_string, Args)) {
+        alias convert_strings!(Args) NewArgs;        
 
-    NewArgs new_args;
+        NewArgs new_args;
 
-    foreach(i, arg; args) {
-        static if(is(typeof(arg) == string) || is(typeof(arg) == wstring)) {
-            new_args[i] = AWEString(arg);
-        } else {
-            new_args[i] = arg;
-        }
-    }
-
-    scope(exit) {
         foreach(i, arg; args) {
             static if(is(typeof(arg) == string) || is(typeof(arg) == wstring)) {
-                new_args[i].free();
+                new_args[i] = AWEString(arg);
+            } else {
+                new_args[i] = arg;
             }
         }
+
+        scope(exit) {
+            foreach(i, arg; args) {
+                static if(is(typeof(arg) == string) || is(typeof(arg) == wstring)) {
+                    new_args[i].free();
+                }
+            }
+        }
+    } else {
+        alias args new_args;
     }
 
     static if(is(ReturnType!func == void)) {
